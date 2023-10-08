db5c7006cfc7271af98ccd94254a5cd858e37950
anisul islam <anisulislam980340@gmail.com>
2023-10-08 23:44:18 +0600

    create employee class

--- users.py
+++ users.py
@@ -1,13 +1,16 @@
 from abc import ABC, abstractmethod
 class User(ABC):
-    def __init__(self, name):
+    def __init__(self, name, phone, email, address):
         self.name=name
+        self.phone= phone
+        self.email= email
+        self.address= address
 
 class Customer(User):
-    def __init__(self, name, money):
+    def __init__(self, name, phone, email, address, money):
         self.wallet= money
         self.__order=None
-        super().__init__(name)
+        super().__init__(name, phone, email, address)
     @property
     def order(self):
         return self.__order
@@ -28,4 +31,30 @@ class Customer(User):
 
     def write_review(self, stars):
         pass
-        
+
+
+class Employee(User):
+    def __init__(self, name, phone, email, address, salary,starting_date, department):
+        self.salary= salary
+        self.department= department
+        self.starting_date= starting_date
+        super().__init__(name, phone, email, address)
+
+class Chef(Employee):
+    def __init__(self, name, phone, email, address, salary, starting_date, department, cooking_item):
+        self.cooking_item= cooking_item
+        super().__init__(name, phone, email, address, salary, starting_date, department)
+
+class Server(Employee):
+    def __init__(self, name, phone, email, address, salary, starting_date, department):
+        super().__init__(name, phone, email, address, salary, starting_date, department)
+        self.tips_earning= 0
+    def take_order(self, order):
+        pass
+
+    def transfer_order(self, order):
+        pass
+    def serve_food(self, order):
+        pass
+    def receive_tips(self, amount):
+        self.tips_earning+=amount
