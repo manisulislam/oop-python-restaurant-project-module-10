1f2262dc6282acb6fd0fc95d371a734bad4c83ad
anisul islam <anisulislam980340@gmail.com>
2023-10-09 18:50:00 +0600

    restaurant

--- Main.py
+++ Main.py
@@ -1,51 +1,74 @@
-from Menu import Burger, Drinks, Pizza, Menu
-from restaurant import Restaurant
-from users import Chef, Customer, Manager, Server
+from Menu import Pizza, Burger, Drinks, Menu
+from Restaurant import Restaurant
+from Users import Chef, Customer, Server, Manager
+from Order import Order
+
 def main():
-    menu= Menu()
-    # pizza
-    pizza_1= Pizza("sutki pizza",600, "large",["sutki", "onion", "oil"])
-    menu.add_menu_item("pizza", pizza_1)
-    pizza_2= Pizza("alor vorta pizza", 420, "big", ["potato", "oil"])
-    menu.add_menu_item("pizza", pizza_2)
-    pizza_3= Pizza("dal er pizza", 500, "small",["dal", "oil"])
-    menu.add_menu_item("pizza", pizza_3)
+    menu = Menu()
+    pizza_1 = Pizza('Shutki Pizza', 600, 'large', ['shutki', 'onion'])
+    menu.add_menu_item('pizza', pizza_1)
+    pizza_2 = Pizza('Alur Vorta Pizza', 400, 'large', ['potato', 'onion', 'oil'])
+    menu.add_menu_item('pizza', pizza_2)
+    pizza_3 = Pizza('Dal Pizza', 500, 'large', ['dal', 'oil'])
+    menu.add_menu_item('pizza', pizza_3)
 
-    # burger
-    burger_1= Burger("naga burger", 520,"chicken",["bread","chili"])
-    menu.add_menu_item("burger", burger_1)
-    burger_2 = Burger("beef burger", 6520, "beef", ["goro", "haddi"])
-    menu.add_menu_item("burger", burger_2)
-    
-    # drinks
-    coke= Drinks("coke", 50, True)
-    menu.add_menu_item("drinks", coke)
-    coffee = Drinks("mocha coffee", 120, False)
-    menu.add_menu_item("drinks",coffee)
+    # add burger to the menu
+    burger_1 = Burger('Naga Burger', 1000, 'chicken', ['bread', 'chili'])
+    menu.add_menu_item('burger', burger_1)
+    burger_2 = Burger('Beef Burger', 1200, 'beef', ['goru', 'haddi'])
+    menu.add_menu_item('burger', burger_2)
 
-    # show menu
-    menu.show_menu()
+    # add drinks to the menu
+    coke = Drinks('Coke', 50, True)
+    menu.add_menu_item('drinks', coke)
+    coffee = Drinks('Mocha Coffee', 300, False)
+    menu.add_menu_item('drinks', coffee)
 
-    # restaurant
-    restaurant= Restaurant("sai baba restaurant",2000, menu)
+    # show Menu
+    # menu.show_menu()
 
-    # add employee
-    manager= Manager("kala chan","032145","ka@gmail.com", "kala pur",2000, "jan 1 2020","core")
+    restaurant = Restaurant('Sai Baba Restaurant', 2000, menu)
 
-    restaurant.add_employee("manager", manager)
+    # add employees
+    manager = Manager('Kala Chan Manager', 5, 'kala@chan.com', 'kaliapur', 1500, 'Jan 1 2020', 'core')
+    restaurant.add_employee('manager', manager)
+    chef = Chef('Rustom Baburchi', 6, 'chupa@rustom.com', 'rustomNagar', 3500, 'Feb 1, 2020', 'Chef', 'everything')
+    restaurant.add_employee('chef', chef)
+    server = Server('Chotu server', 6, 'nai@jai.com', 'restaurant', 200, 'March 1, 2020', 'server')
+    restaurant.add_employee('server', server)
 
-    chef= Chef("rostom", "526156", "ros@gmail.com","india",4000,"february 2 2020","chef","everything")
-    restaurant.add_employee("chef",chef)
-    
-    server=Server("pakhi bai", "41526","paki@gamil.com","pakistan",1450,"june 2 2020","server")
-    restaurant.add_employee("server", server)
+    # show employees
+    # restaurant.show_employees()
 
-    # show employee
-    restaurant.show_employee()
+    # customer 1 placing an order
+    customer_1 = Customer('Sakib Khan', 6, 'king@khan.com', "banani", 100000)
+    order_1 = Order(customer_1, [pizza_3, coke, burger_1, pizza_3, coffee])
+    customer_1.pay_for_order(order_1)
+    restaurant.add_order(order_1)
+    # customer one paying for order_1
+    restaurant.receive_payment(order_1, 20000, customer_1)
+
+    print('revenue and balance after first customer', restaurant.revenue, restaurant.balance)
+
+    # customer 2 placing an order
+    customer_2 = Customer('Sakib Al Hasan', 6, 'king@khan.com', "banani", 100000)
+    order_2 = Order(customer_2, [pizza_1, burger_2, burger_1, pizza_2, coffee])
+    customer_2.pay_for_order(order_2)
+    restaurant.add_order(order_2)
+    # customer one paying for order_1
+    restaurant.receive_payment(order_2, 10000, customer_2)
+
+    print('revenue and balance after second customer', restaurant.revenue, restaurant.balance)
+
+    # pay rent
+    restaurant.pay_expense(restaurant.rent, 'Rent')
+    print('after rent', restaurant.revenue, restaurant.balance, restaurant.expense)
+
+    restaurant.pay_salary(chef)
+    print('after salary', restaurant.revenue, restaurant.balance, restaurant.expense)
 
 
 
-
-
-if __name__ == "__main__":
+# call the main 
+if __name__ == '__main__':
     main()
--- Menu.py
+++ Menu.py
@@ -1,53 +1,51 @@
-class Food():
+class Food:
     def __init__(self, name, price) -> None:
-        self.name= name
-        self.price= price
-        self.cooking_time=15
-    
+        self.name = name
+        self.price = price
+        self.cooking_time = 15
 
 class Burger(Food):
     def __init__(self, name, price, meat, ingredients) -> None:
-        self.meat= meat
-        self.ingredients= ingredients
         super().__init__(name, price)
+        self.meat = meat
+        self.ingredients = ingredients
+
 
 class Pizza(Food):
     def __init__(self, name, price, size, toppings) -> None:
-        self.size= size
-        self.toppings= toppings
         super().__init__(name, price)
+        self.size = size
+        self.toppings = toppings
 
 class Drinks(Food):
-    def __init__(self, name, price, isCold= True) -> None:
-        self.isCold= isCold
+    def __init__(self, name, price, isCold = True) -> None:
         super().__init__(name, price)
+        self.isCold = isCold
 
 
 # composition
 class Menu:
     def __init__(self) -> None:
-        self.pizzas= []
-        self.drinks=[]
-        self.burgers=[]
+        self.pizzas = []
+        self.burgers = []
+        self.drinks = []
 
     def add_menu_item(self, item_type, item):
-        if item_type == "pizza":
+        if item_type == 'pizza':
             self.pizzas.append(item)
-        elif item_type == "drinks":
-            self.drinks.append(item)
-        elif item_type == "burger":
+        elif item_type == 'burger':
             self.burgers.append(item)
-        
-    def remove_pizza(self,pizza):
+        elif item_type == 'drinks':
+            self.drinks.append(item)
+
+    def remove_pizza(self, pizza):
         if pizza in self.pizzas:
             self.pizzas.remove(pizza)
-    
 
     def show_menu(self):
         for pizza in self.pizzas:
-            print(f"name: {pizza.name} and price : {pizza.price}")
-        
+            print(f'name: {pizza.name} price: {pizza.price}')
         for burger in self.burgers:
-            print(f"name : {burger.name} and price : {burger.price}")
+            print(f'name: {burger.name} price: {burger.price}')
         for drink in self.drinks:
-            print(f"name : {drink.name} and price : {drink.price}")
+            print(f'name: {drink.name} price: {drink.price}')
